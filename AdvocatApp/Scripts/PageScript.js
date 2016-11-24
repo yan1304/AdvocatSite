379c6f548aaf3226075343450ffbb838855c08dd
yan1304 <evtushenko1304@mail.ru>
2016-11-24 17:53:11 +0300

    add jQuery links

--- AdvocatApp/Scripts/PageScript.js
+++ AdvocatApp/Scripts/PageScript.js
@@ -1,7 +1,9 @@
 /// <reference path="typings/jquery/jquery.d.ts" />
 window.onload = function () {
-    var s = document.getElementById("textSt")
-        .getAttribute("value");
-    document.getElementById("textSite").innerHTML = s;
+    //let s: string = document.getElementById("textSt")
+    //    .getAttribute("value");
+    //document.getElementById("textSite").innerHTML = s;
+    var statie = $("#textSt").val();
+    $("#textSite").html(statie);
 };
 //# sourceMappingURL=PageScript.js.map
--- AdvocatApp/Scripts/PageScript.ts
+++ AdvocatApp/Scripts/PageScript.ts
@@ -1,7 +1,9 @@
 ﻿/// <reference path="typings/jquery/jquery.d.ts" />
 
 window.onload = () => {
-    let s: string = document.getElementById("textSt")
-        .getAttribute("value");
-    document.getElementById("textSite").innerHTML = s;
+    //let s: string = document.getElementById("textSt")
+    //    .getAttribute("value");
+    //document.getElementById("textSite").innerHTML = s;
+    let statie: string = $("#textSt").val();
+    $("#textSite").html(statie);
 };
